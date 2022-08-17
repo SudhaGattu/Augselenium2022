a21d9d77bd6b1faa063792dfe7ed1f9f10d40541
SudhaGattu <sudhagattu@yahoo.com>
2022-08-17 00:00:21 -0400

    First project

---
+++ src/test/java/com/java/FirstJavaProgram.java
@@ -0,0 +1,10 @@
+package com.java;
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Sudha. How are you");
+	}
+
+}
